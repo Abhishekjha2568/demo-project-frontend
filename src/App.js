f5b29e657c0b08aef22a10f587c4430a5dd51d4d
Delta Student <kumarabhishekkjr@gmail.com>
2026-01-22 21:45:51 +0530

    fix react router issue

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Signup from './pages/Signup';
-import VerifyOTP from './pages/VerifyOTP';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import VerifyOTP from "./pages/VerifyOTP";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Signup />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/verify-otp" element={<VerifyOTP />} />
-        <Route path="/login" element={<Login />} />  
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
